7f3cc654c7804adc3193d60d24958920b0d41571
Adrian <adrian.dev@kousz.ch>
2026-01-29 00:52:58 +0100

    bundle all as dll

--- src/Hello.cs
+++ src/Hello.cs
@@ -1,29 +1,9 @@
 using System.IO;
-using System.Reflection;
 using KeePass.DataExchange;
 using KeePass.Resources;
 using KeePassLib;
 using KeePassLib.Interfaces;
 using KeePassLib.Security;
-
-[assembly: System.Reflection.AssemblyTitle("HelloWorld")]
-[assembly: System.Reflection.AssemblyDescription ("Generates a Hello World entry")]
-[assembly: System.Reflection.AssemblyProduct("KeePass Plugin")]
-[assembly: System.Reflection.AssemblyVersion ("0.1.2")]
-
-namespace HelloWorld
-{
-	public class HelloWorldExt : KeePass.Plugins.Plugin
-	{
-		public override string UpdateUrl { get { return "https://example.com/update.txt"; } }
-
-		public override bool Initialize(KeePass.Plugins.IPluginHost host)
-		{
-			host.FileFormatPool.Add(new Adrium.Snippets.HelloWorldFormatProvider());
-			return true;
-		}
-	}
-}
 
 namespace Adrium.Snippets
 {
