82b4d8a1cef62af43a432ecd14ff0c5fa2f72763
Adrian <adrian.dev@kousz.ch>
2026-01-28 23:28:00 +0100

    fix

--- src/Hello.cs
+++ src/Hello.cs
@@ -11,25 +11,25 @@ using KeePassLib.Security;
 
 namespace Adrium.Snippets
 {
-    public class HelloWorldFormatProvider : FileFormatProvider
-    {
-        public override bool SupportsImport => true;
-        public override bool SupportsExport => false;
-        public override bool ImportAppendsToRootGroupOnly => true;
-        public override bool RequiresFile => false;
-        public override string ApplicationGroup => KPRes.PasswordManagers;
-        public override string FormatName => "The Hello World Entry";
+	public class HelloWorldFormatProvider : FileFormatProvider
+	{
+		public override bool SupportsImport => true;
+		public override bool SupportsExport => false;
+		public override bool ImportAppendsToRootGroupOnly => true;
+		public override bool RequiresFile => false;
+		public override string ApplicationGroup => KPRes.PasswordManagers;
+		public override string FormatName => "The Hello World Entry";
 
-        public override void Import(PwDatabase pwStorage, Stream sInput, IStatusLogger slLogger)
-        {
-            var entry = new PwEntry(true, true);
+		public override void Import(PwDatabase pwStorage, Stream sInput, IStatusLogger slLogger)
+		{
+			var entry = new PwEntry(true, true);
 
-            entry.Strings.Set(PwDefs.TitleField, new ProtectedString(false, "Hello World"));
-            entry.Strings.Set(PwDefs.UserNameField, new ProtectedString(false, "hello"));
-            entry.Strings.Set(PwDefs.PasswordField, new ProtectedString(true, "world"));
+			entry.Strings.Set(PwDefs.TitleField, new ProtectedString(false, "Hello World"));
+			entry.Strings.Set(PwDefs.UserNameField, new ProtectedString(false, "hello"));
+			entry.Strings.Set(PwDefs.PasswordField, new ProtectedString(true, "world"));
 
-            pwStorage.RootGroup.AddEntry(entry, true);
-            slLogger.SetText("Hello World Entry added", LogStatusType.Info);
-        }
-    }
+			pwStorage.RootGroup.AddEntry(entry, true);
+			slLogger.SetText("Hello World Entry added", LogStatusType.Info);
+		}
+	}
 }
