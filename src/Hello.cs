a27b9e69ad179ace1271152df59d3f3717b18fc6
Adrian <adrian.dev@kousz.ch>
2026-01-28 23:19:48 +0100

    KeePass Hello World

---
+++ src/Hello.cs
@@ -0,0 +1,35 @@
+using System.IO;
+using System.Reflection;
+using KeePass.DataExchange;
+using KeePass.Resources;
+using KeePassLib;
+using KeePassLib.Interfaces;
+using KeePassLib.Security;
+
+[assembly: AssemblyTitle("HelloWorld FormatProvider")]
+[assembly: AssemblyVersion ("0.1.2")]
+
+namespace Adrium.Snippets
+{
+    public class HelloWorldFormatProvider : FileFormatProvider
+    {
+        public override bool SupportsImport => true;
+        public override bool SupportsExport => false;
+        public override bool ImportAppendsToRootGroupOnly => true;
+        public override bool RequiresFile => false;
+        public override string ApplicationGroup => KPRes.PasswordManagers;
+        public override string FormatName => "The Hello World Entry";
+
+        public override void Import(PwDatabase pwStorage, Stream sInput, IStatusLogger slLogger)
+        {
+            var entry = new PwEntry(true, true);
+
+            entry.Strings.Set(PwDefs.TitleField, new ProtectedString(false, "Hello World"));
+            entry.Strings.Set(PwDefs.UserNameField, new ProtectedString(false, "hello"));
+            entry.Strings.Set(PwDefs.PasswordField, new ProtectedString(true, "world"));
+
+            pwStorage.RootGroup.AddEntry(entry, true);
+            slLogger.SetText("Hello World Entry added", LogStatusType.Info);
+        }
+    }
+}
